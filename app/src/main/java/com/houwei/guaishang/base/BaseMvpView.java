77bca81635b77f14d7e140185c22bcb81f44f214
bearLei <18850415334.com>
2018-04-23 14:39:44 +0800

    提交一个base包

---
+++ app/src/main/java/com/houwei/guaishang/base/BaseMvpView.java
@@ -0,0 +1,8 @@
+package com.houwei.guaishang.base;
+
+/**
+ * Created by lenovo on 2017/12/30.
+ */
+
+public interface BaseMvpView<T> {
+}
---
+++ app/src/main/java/com/houwei/guaishang/base/holder/HolderParent.java
@@ -0,0 +1,8 @@
+package com.houwei.guaishang.base.holder;
+
+/**
+ * Created by lei on 2017-12-26. <br/>
+ */
+public interface HolderParent {
+
+}
